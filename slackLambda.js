84ba3cc193d24a9b9be5fa8e42d9ad853e31bfea
Andrea <marchetti.andrea@gmail.com>
2018-06-01 17:06:29 +0200

    a lambda function dynamo mockup

---
+++ slackLambda.js
@@ -0,0 +1,31 @@
+console.log('Loading function')
+
+exports.handler = async (event, context) => {
+  // Load the AWS SDK for Node.js
+  var AWS = require('aws-sdk')
+
+  AWS.config.update({region: 'eu-central-1'})
+
+  const userId = event.userId
+  const messageLength = event.messageLength
+
+  // Create the DynamoDB service object
+  const ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'})
+
+  var params = {
+    TableName: 'workshopSlack',
+    Item: {
+      userId: userId,
+      messageLegth: messageLength
+    }
+  }
+
+  // Call DynamoDB to add the item to the table
+  ddb.putItem(params, (err, data) => {
+    if (err) {
+      console.log('Error', err)
+    } else {
+      console.log('Success', data)
+    }
+  })
+}
